010704df69f7d6fc4f5cf3d4f1bd80cf3c18f69f
AbdulSattar2010 <sattar2k10@gmail.com>
2021-12-05 22:25:34 +0500

    Initial Commit

---
+++ Converter/ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConverterApp.swift
+//  Converter
+//
+//  Created by muhammad hassan on 05/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
